52ec8b8c68737b63a52a30e7ef4fab68e53964a8
牟霄宇 <1102828300@qq.com>
2019-10-07 17:06:34 +0800

    hw

---
+++ 牧师与魔鬼动作分离版/Assets/Scripts/Controller.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using mygame;
+
+namespace mygame
+{
+
+    public enum State { Start, SE_move, ES_move, End, Win, Lose };
+
+    public interface UserAction {
+
+        void priestSOnB();
+
+        void priestEOnB();
+
+        void devilSOnB();
+
+        void devilEOnB();
+
+        void moveShip();
+
+        void offShipL();
+
+        void offShipR();
+
+        void reset();
+
+    }
+ 
+    public class SSDirector : System.Object, UserAction
+    {
+        private static SSDirector _instance;
+        public Controller currentScenceController;
+        public State state = State.Start;
+        private Model game_obj; 
+
+        public static SSDirector GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new SSDirector();
+            }
+            return _instance;
+        }
+ 
+        public Model getModel()
+        {
+            return game_obj;
+        }
+        
+        internal void setModel(Model someone)
+        {
+            if(game_obj == null)
+            {
+                game_obj = someone;
+            }
+        }
+
+        public void priestSOnB()
+        {
+            game_obj.priS();
+        }
+
+        public void priestEOnB()
+        {
+            game_obj.priE();
+        }
+
+        public void devilSOnB()
+        {
+            game_obj.delS();
+        }
+
+        public void devilEOnB()
+        {
+            game_obj.delE();
+        }
+
+        public void moveShip()
+        {
+            game_obj.moveShip();
+        }
+
+        public void offShipL()
+        {
+            game_obj.getOffTheShip(0);
+        }
+
+        public void offShipR()
+        {
+            game_obj.getOffTheShip(1);
+        }
+
+        public void reset()
+        {
+	state = State.Start;
+                game_obj.Reset_all();
+        }
+}
+       
+}
+
+public class Controller : MonoBehaviour {
+    // Use this for initialization
+    void Start()
+    {
+        SSDirector one = SSDirector.GetInstance();
+    }
+}
---
+++ 牧师与魔鬼动作分离版/README.md
@@ -0,0 +1,2 @@
+# -
+作业四详解博客见https://blog.csdn.net/yuqibaba/article/details/102078659
